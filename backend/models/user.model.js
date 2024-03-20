74b17e1d8d68fd30e704a604a673718ff762506f
deepaksharmadk <dks847203@gmail.com>
2024-03-20 17:32:35 +0530

    update user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,28 +1,42 @@
 import mongoose from "mongoose";
+
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      require: true,
+      required: [true, "Please enter your Name!"],
+      minLength: [3, "Name must contain at least 3 Characters!"],
+      maxLength: [30, "Name cannot exceed 30 Characters!"],
     },
     email: {
       type: String,
-      required: true,
-      unique: true,
+      required: [true, "Please enter your Email!"],
+      validate: [validator.isEmail, "Please provide a valid Email!"],
     },
-    mobile: {
+    phone: {
       type: Number,
-      required: true,
-      unique: true,
+      required: [true, "Please enter your Phone Number!"],
     },
     password: {
       type: String,
-      required: true,
+      required: [true, "Please provide a Password!"],
+      minLength: [8, "Password must contain at least 8 characters!"],
+      maxLength: [32, "Password cannot exceed 32 characters!"],
+      select: false,
+    },
+    role: {
+      type: String,
+      required: [true, "Please select a role"],
+      enum: ["Job Seeker", "Employer"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
   },
   {
     timestamps: true,
+    versionKey: false,
   }
 );
-const User = mongoose.model("User", userSchema);
-export default User;
+export const User = mongoose.model("User", userSchema);
